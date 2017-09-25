e79bc94ae42a8f5b00194d98ab921fddab750908
Deonis Peretyagin <karudo@gmail.com>
2017-09-25 22:55:29 +0700

    fix filenames

--- scripts/fpj.js
+++ scripts/fpj.js
@@ -4,15 +4,15 @@ var fs = require('fs');
 var package = require(path.join(__dirname, '../package.json'));
 
 delete package.babel;
-delete package["lint-staged"];
+delete package['lint-staged'];
 delete package.jest;
 delete package.bundlesize;
-delete package["pre-commit"];
+delete package['pre-commit'];
 delete package.scripts;
 delete package.options;
 
-package.main = 'react-apollo.umd.js';
-// package.browser = 'react-apollo.browser.umd.js';
+package.main = 'xenk.umd.js';
+// package.browser = 'xenk.browser.umd.js';
 package.module = 'index.js';
 package['jsnext:main'] = 'index.js';
 package.typings = 'index.d.ts';
